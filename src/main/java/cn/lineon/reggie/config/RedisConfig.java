ebaf8d8444578b943cf39904a7775467a02b9a7b
lanxi <85605964@qq.com>
2022-06-13 16:02:13 +0800

    Redis测试

---
+++ src/main/java/cn/lineon/reggie/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package cn.lineon.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis配置类
+ * Redis序列化配置类，用于解决Redis key 乱码问题
+ */
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        //Value序列化设置
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+
+}
